9f9f0adeaf6396973203a0790af46d2fcefc09b6
HSNMUH <hsnmuh47@gmail.com>
2020-03-03 15:45:13 -0500

    Change SerialPort_test

--- SerialPort_test.java
+++ SerialPort_test.java
@@ -17,16 +17,6 @@ public class SerialPort_test {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        SerialPort comPort = SerialPort.getCommPorts()[0];
-        comPort.openPort();
-        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-        InputStream in = comPort.getInputStream();
-        try
-        {
-           for (int j = 0; j < 1000; ++j)
-              System.out.print((char)in.read());
-           in.close();
-        } catch (Exception e) { e.printStackTrace(); }
-            comPort.closePort();
-        }  
+        PortModule ard = new PortModule();
+        System.out.println(ard.printNTimes(10));
 }
