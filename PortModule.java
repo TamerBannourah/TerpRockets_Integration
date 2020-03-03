7540b745bc8c1e6fb8cd62dead305467743ac544
Hasan Muhammad <hsnmuh47@gmail.com>
2020-03-03 16:50:04 -0500

    Update PortModule.java

--- PortModule.java
+++ PortModule.java
@@ -11,13 +11,14 @@ public class PortModule {
 	private boolean open;
 	
 	public PortModule() {
-		comPort = SerialPort.getCommPorts()[0];
-        	comPort.openPort();
-        	comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-        	in = comPort.getInputStream();
-        	open = true;
+		comPort = SerialPort.getCommPorts()[0]; 
+        comPort.openPort();
+       	comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+       	in = comPort.getInputStream();
+       	open = true;
 	}
 	
+	/* Close the port when done with the module*/
 	public void closePort() {
 		open = false;
 		comPort.closePort();
@@ -27,21 +28,25 @@ public class PortModule {
 		return in;
 	}
 	
-	public ArrayList<Character> readNTimes(int n) {
-		if(open == false)
-			throw new Exception("Port is closed!");
+	/* Reads n characters from the serial port stream and returns the results in an Array */
+	public ArrayList<Character> readNTimes(int n) throws Exception{
+		if(open == false) {//Check that the port is open for reading
+			System.out.println("Port is closed!");
+			return null;
+		}
 		ArrayList<Character> ret = new ArrayList<Character>();
 		for(int i = 0; i < n; i++) {
 			try {
 				ret.add((char)in.read());
 			} catch (IOException e) {
-				comPort.closePort();
+				comPort.closePort(); //If something goes wrong, close the port
 				e.printStackTrace();
 			}
 		}
 		return ret;
 	}
 	
+	/* IDEAS FOR LATER MAYBE */
 	// Read every n seconds???
 	// Data remaining???
 }
