84d84acebdbddd6ad399f1527da68d5e0b47578b
Hasan Muhammad <hsnmuh47@gmail.com>
2020-03-03 15:24:52 -0500

    Add files via upload

---
+++ PortModule.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+public class PortModule {
+	
+	private InputStream in;
+	private SerialPort comPort;
+	
+	public PortModule() {
+		comPort = SerialPort.getCommPorts()[0];
+        comPort.openPort();
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+        in = comPort.getInputStream();
+	}
+	
+	public ArrayList<Character> readNTimes(int n) {
+		ArrayList<Character> ret = new ArrayList<Character>();
+		for(int i = 0; i < n; i++) {
+			try {
+				ret.add((char)in.read());
+			} catch (IOException e) {
+				comPort.closePort();
+				e.printStackTrace();
+			}
+		}
+		return ret;
+	}
+	
+	// Read every n seconds???
+	// Data remaining???
+}
